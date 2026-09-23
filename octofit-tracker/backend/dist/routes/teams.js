4aec4c6b95312f2d4bc06138f17e098441b718e7
SamCastroMX <98236705+SamCastroMX@users.noreply.github.com>
2026-09-23 21:52:56 +0000

    refactor: remove backend configuration and model files

--- octofit-tracker/backend/dist/routes/teams.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.teamsRouter = void 0;
-const Team_js_1 = require("../models/Team.js");
-const modelRouter_js_1 = require("./modelRouter.js");
-exports.teamsRouter = (0, modelRouter_js_1.createModelRouter)(Team_js_1.Team);
--- octofit-tracker/backend/dist/routes/users.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.usersRouter = void 0;
-const User_js_1 = require("../models/User.js");
-const modelRouter_js_1 = require("./modelRouter.js");
-exports.usersRouter = (0, modelRouter_js_1.createModelRouter)(User_js_1.User);
